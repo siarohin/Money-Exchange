bcdfedfe7a5b3b25d05204b509cc62e2a8acb321
A.Seregin <a.siarohin@gmail.com>
2018-09-23 10:47:23 +0300

    Test #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+ 
+// Coins used will be half-dollars, quarters, dimes, nickels, and pennies, worth 50¢, 25¢, 10¢, 5¢ and 1¢, respectively.
+// They'll be represented by the strings H, Q, D, N and P.
+
+	var currency,
+		Error = {
+			error: "You are rich, my friend! We don't have so much coins for exchange"
+		};
+
+	function makeExchange() {
+		if (currency > 10000) {
+			return Error;
+		} else return currency;
+	}
+
+	return makeExchange();
 }
