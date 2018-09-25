2a1bb95ee9a45edb84afcf0c8457806847eda9c3
A.Seregin <a.siarohin@gmail.com>
2018-09-25 23:08:49 +0300

    20 tests

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
 // PLEASE DON'T change function name
-;module.exports = function makeExchange(currency) {
+module.exports = function makeExchange(currency) {
 
 		var currency,
-
 		 	coins = [50, 25, 10, 5, 1],
-
 			str = ['H', 'Q', 'D', 'N', 'P'],
-
 			cash = {},
-
-			Error = {
-			error: "You are rich, my friend! We don't have so much coins for exchange"
-		};
+			Error = {error: "You are rich, my friend! We don't have so much coins for exchange"};
 
 
-		if (currency > 10000) {
-			return Error; 
-		} 
+		if (currency > 10000) return Error; 
+		if (currency <= 0) return {}; 
 
-		var i = 0;
+		i = 0;
 		while (currency != 0 && i < coins.length) {
 			
-			var result = currency / coins[i];
-				currency -= Math.floor(result) * coins[i];
-			var cashStr = Math.floor(result);
+		    result = currency / coins[i];
+			currency -= Math.floor(result) * coins[i];
+			cashStr = Math.floor(result);
 
 			if (cashStr != 0) {
 	            cash[str[i]] = Math.floor(result); 
-	        }
-
+	        	}
         i++;
 
 		}
